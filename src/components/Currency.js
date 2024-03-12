5c95bc0e06b72946ff4161239350041b148053b4
LadyTano <sarah.tannert@gmail.com>
2024-03-11 20:07:18 -0400

    Almost there

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,14 +1,15 @@
 
 import React from 'react';
-import { useState } from 'react';
+//import { useState } from 'react';
 
-const white = {backgroundColor: "white"};
-const lightGreen = {backgroundColor: "lightGreen"};
+//const white = {backgroundColor: "white"};
+//const lightGreen = {backgroundColor: "lightGreen"};
 
 export default function CurrencyMenu ()
 {
 
-    const [color, setColor] = useState(lightGreen);
+//const [color, setColor] = useState(true);
+
 
     const handleChange = () => {
         const currencySymbol = document.getElementById("dropdown");
@@ -17,21 +18,27 @@ export default function CurrencyMenu ()
         
     }
 
-   
+    function MouseOver(event) {
+        event.target.style.background = 'white';
+        console.log("Hover")
+      }
+      function MouseOut(event){
+        event.target.style.background="lightgreen";
+      }
     
     return (
    
-      <div>
+      <div >
    
-        <select style={color} onMouseEnter={() => setColor} onChange={handleChange} id = "dropdown">
-   
-        <option   value="£"  >£ Pound</option>
+        <select size = "4" onMouseOver={MouseOver}  onMouseOut={MouseOut} onChange={handleChange} id = "dropdown" >
+        
+        <option  value="£"  >£ Pound</option>
 
-          <option value="$">$ Dollar</option>
+          <option  value="$" onMouseOver={MouseOver} onMouseOut={MouseOut}>$ Dollar</option>
    
-          <option value="€">€ Euro</option>
+          <option  value="€" onMouseOver={MouseOver} onMouseOut={MouseOut}>€ Euro</option>
           
-          <option value="₹">₹ Ruppee</option>
+          <option  value="₹" onMouseOver={MouseOver} onMouseOut={MouseOut}>₹ Ruppee</option>
    
         </select>
 
